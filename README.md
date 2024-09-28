8dc7449122544459f1b5a903d53b123dec51197d
vondrakmar <vondrakmar@gmail.com>
2024-09-28 15:09:50 +0200

    Matmul

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # mathc
+I realized I have not coded in C for really long time. This is my attempt. It is shit, and you should leave now
--- main.c
+++ main.c
@@ -1,49 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
 
 
-int load_size_vector(char *file_name) {
-    FILE *file;
-    int n_vec;
 
-    file = fopen(file_name, "r");
-    if (file == NULL) {
-        perror("Error opening file");
-        return -1; 
-    }
-    if (fscanf(file, "%d", &n_vec) != 1) {
-        fclose(file);
-        return -1;
-    }
-    fclose(file);
-    return n_vec;
+
+double random_number(int min_value,int max_value){
+    double range = (max_value - min_value);
+    double div = RAND_MAX  / range;
+    double r_n = min_value + (rand() / div);
+    return r_n;
 }
 
-void load_vector(char *file_name,double* vec){ 
-    
-    FILE *ptr = fopen(file_name, "r");
-    if (ptr == NULL) {
-        perror("Error opening file");
-        return;
+void init_random_vector(double *vec, int size){
+    for (int i = 0 ; i < size; i++){
+        vec[i] = random_number(-10,10);
     }
+}
 
-    int n;
-    if (fscanf(ptr, "%d", &n) != 1) {
-        perror("Error reading number of elements");
-        fclose(ptr);
-        return;
-    }
-
-    // Read the elements into the vector
-    for (int i = 0; i < n; i++) {
-        if (fscanf(ptr, "%lf", &vec[i]) != 1) { 
-            perror("Error reading vector element");
-            fclose(ptr);
-            return;
+void init_random_matrix(double **mat, int dim1,int dim2){
+    for (int i = 0 ; i < dim1; i++){
+        for (int j = 0 ; j < dim2 ; j++){
+            mat[i][j] = random_number(-10,10);
         }
     }
-   
 }
 
 double dot_product(double* vec1, double* vec2, int size){
@@ -54,6 +35,22 @@ double dot_product(double* vec1, double* vec2, int size){
     return result;
 }
 
+double** matmul(double** A,double** B, int dimA, int dim_both, int dimB){
+    double **resM = (double**)malloc(dimA*sizeof(*resM));
+    for (int i = 0; i < dimA; i++){
+        resM[i] = (double*)malloc(dimB*sizeof(double));
+    }
+    for (int rowA =0 ; rowA < dimA; rowA++){
+        for (int colB = 0 ; colB < dimB ; colB++){
+            resM[rowA][colB] = 0;
+            for (int com = 0 ; com < dim_both ; com++){
+                resM[rowA][colB] += A[rowA][com]*B[com][colB];
+            }
+        }
+    }
+    return resM;
+}
+    
 void print_vector(double* vec,int size){
     for (int i = 0; i < size; i++) {
         printf("%lf ", vec[i]);
@@ -61,29 +58,43 @@ void print_vector(double* vec,int size){
     printf("\n");
 }
 
-void do_dot_product(char *file_vec1, char *file_vec2){
-    int n_vec1 = load_size_vector(file_vec1);
-    if (n_vec1 == -1){
-        printf("vector failed to load");
-        return;
+void print_matrix(double **mat, int dim1,int dim2){
+    for (int i = 0 ; i < dim1; i++){
+        for (int j = 0 ; j < dim2 ; j++){
+            printf("%lf ", mat[i][j]);
+        }
+        printf("\n");
     }
-    double *vec1 = (double*)calloc(n_vec1,sizeof(double));
-    load_vector(file_vec1,vec1);
-    int n_vec2 = load_size_vector(file_vec2);
-    if (n_vec2 == -1){
-        printf("vector failed to load");
-        return;
-    }
-    double *vec2 = (double*)calloc(n_vec1,sizeof(double));
-    load_vector(file_vec2,vec2);
-    double dot12 = dot_product(vec1,vec2,n_vec1);
-    printf("%lf\n",dot12);
-    free(vec1);
-    free(vec2);
+    printf("\n");
 }
 
 int main(){
-    do_dot_product("vec1","vec2");
+    srand(time(NULL));
+    /* int n_vec1 = 10; */
+    /* double *vec1 = (double*)calloc(n_vec1,sizeof(double)); */
+    /* init_random_vector(vec1,n_vec1); */
+    /* print_vector(vec1,n_vec1); */
+    int dimA1 = 4;
+    int dimA2 = 6;
+    /* Init rows */
+    double **matrix1 = (double**)malloc(dimA1*sizeof(*matrix1));
+    for (int i = 0; i < dimA1; i++){
+        /* Init cols */
+        matrix1[i] = (double*)malloc(dimA2*sizeof(double));
+    }
+    init_random_matrix(matrix1,dimA1,dimA2);
+    int dimB1 = 6;
+    int dimB2 = 3;
+    double **matrix2 = (double**)malloc(dimB1*sizeof(*matrix1));
+    for (int i = 0; i < dimB1; i++){
+        matrix2[i] = (double*)malloc(dimB2*sizeof(double));
+    }
+    init_random_matrix(matrix2,dimB1,dimB2);
+    print_matrix(matrix1,dimA1,dimA2);
+    printf("############################\n");
+    print_matrix(matrix2,dimB1,dimB2);
+    double **matmul_result = matmul(matrix1,matrix2,dimA1,dimA2,dimB2);
     
+    print_matrix(matmul_result,dimA1,dimB2);
     return 0;
 }
