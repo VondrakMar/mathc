6ba4af6bfaaa32983b2bd0c60f05d64d517757bb
vondrakmar <vondrakmar@gmail.com>
2025-01-11 15:33:48 +0100

    Simple LU decomposition for a (n,n) matrix

--- src/main.c
+++ src/main.c
@@ -78,25 +78,86 @@ double trace(double** A, size_t rows, size_t cols) {
     return trace;
 }
 
-//void LUdcmp(double** A, int n, ){
+/* void LUdcmp(double** A, double** P, double** L, double** U, size_t dimA1, size_t dimA2){ */
 
-//}
+double** init_U(size_t dimA1, size_t dimA2){
+    // I am passing both parameters now, so I don't need to think, which one I need 
+    double** U = alloc_2d_array(dimA2, dimA2);
+    return U;
+}
+
+double** calloc_2d_array(size_t rows, size_t cols){
+    double **array2D = (double**)calloc(rows, sizeof(*array2D));
+    for (size_t i = 0; i < rows; i++) {
+        array2D[i] = (double*)calloc(cols, sizeof(double));
+    }
+    return array2D;
+}
+
+void LUdcmp(){
+    size_t dim = 3;
+    size_t i,j,k;
+    double** U = calloc_2d_array(dim, dim);
+    double** L = calloc_2d_array(dim, dim);
+    double** P = calloc_2d_array(dim, dim);
+    double** A = calloc_2d_array(dim, dim);
+    read_matrix("A",A,dim,dim); 
+    print_matrix(A,dim,dim);
+    for (i = 0; i < dim; i++){
+        L[i][i] = 1;
+    }
+    double sum;
+    for (j = 0; j < dim; j++){
+        for(i = 0; i <= j; i++){
+            U[i][j] = A[i][j];
+            for (k = 0; k < i; k++){
+                U[i][j] -= L[i][k]*U[k][j];
+            }
+        }
+        for (i = j; i < dim; i++){
+            L[i][j] = A[i][j];
+            for (k = 0; k < j; k++){
+                L[i][j] -= L[i][k]*U[k][j];
+            }
+            L[i][j] = (1/U[j][j])*L[i][j];
+        }
+    }
+    double** t = matmul(L, U, 3, 3, 3);
+    print_matrix(L, 3, 3);
+    print_matrix(U, 3, 3);
+    printf("matrix t\n");
+    print_matrix(t, 3, 3);
+    printf("matrix A\n");
+    print_matrix(A, 3, 3);
+    free_2d_array(P,dim);
+    free_2d_array(A,dim);
+    free_2d_array(L,dim);
+    free_2d_array(U,dim);
+    
+}
 
 
 int main(){
+    LUdcmp();
+    
     /* test_poly(); */
-    size_t n = 3;
-    size_t m = 2;
-    double** U = alloc_2d_array(3,3);
-    double** L = alloc_2d_array(4,3);
-    read_matrix("U",U,3,3);
-    read_matrix("L",L,4,3);
-    double** LU = matmul(L,U, 4, 3, 3); 
-    print_matrix(U,3,3);
-    print_matrix(L,4,3);
-    print_matrix(LU,4,3);
-    free_2d_array(L,4);
-    free_2d_array(U,3);
-    free_2d_array(LU,4);
+    /* size_t n = 3; */
+    /* size_t m = 2; */
+    /* double** U = alloc_2d_array(3,3); */
+    /* double** mal_arr = alloc_2d_array(3,3); */
+    /* double** cal_arr = calloc_2d_array(3,3); */
+    /* print_matrix(mal_arr, 3, 3); */
+    /* printf("#####################\n"); */
+    /* print_matrix(cal_arr, 3, 3); */
+    /* double** L = alloc_2d_array(4,3); */
+    /* read_matrix("U",U,3,3); */
+    /* read_matrix("L",L,4,3); */
+    /* double** LU = matmul(L,U, 4, 3, 3);  */
+    /* print_matrix(U,3,3); */
+    /* print_matrix(L,4,3); */
+    /* print_matrix(LU,4,3); */
+    /* free_2d_array(L,4); */
+    /* free_2d_array(U,3); */
+    /* free_2d_array(LU,4); */
     return 0;
 }
